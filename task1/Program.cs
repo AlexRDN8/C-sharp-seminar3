cc8f93003ccfc85392a39bb0e4d24beeea40d02d
AlexRDN8 <alexrodin8@bk.ru>
2022-12-01 19:48:52 +0300

    added gitignore

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Решение в группах задач:
+Задача 18: Напишите программу, которая по заданному номеру
+четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+bool isParsedQuarter = int.TryParse(Console.ReadLine(), out int Quarter);
+if (!isParsedQuarter)
+{
+    Console.WriteLine("Номер четверти задается числом");
+    return;
+}
+
+int GetCoordinatesByQuarter (int Quarter)
